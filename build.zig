f7f05ed0f54fb468804470ee85976176fe8eae57
Janis Gertulla <j.gertulla@proton.me>
2024-09-20 20:15:21 +0200

    oops, make this a module

--- build.zig
+++ build.zig
@@ -4,13 +4,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-xml",
+    _ = b.addModule("root", .{
         .root_source_file = b.path("src/root.zig"),
-        .target = target,
         .optimize = optimize,
+        .target = target,
     });
-    b.installArtifact(lib);
 
     const bench = b.addExecutable(.{
         .name = "bench",
